3dfc36db36996d36e09ad2481b3c91dc6caf60ae
noyrosenbaum <noyrosen123@gmail.com>
2022-12-14 21:25:13 +0200

    Establish TCP connection with sockets on both sides

--- Ex3/Receiver.c
+++ Ex3/Receiver.c
@@ -1,48 +1,55 @@
 #include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
 #include <string.h>
 #include <arpa/inet.h>
+#include <errno.h>
+#include <signal.h>
 #define SERVER_PORT 8080
 #define SERVER_IP "127.0.0.1"
 #define BUFFER_SIZE 1024
 
 int main()
 {
-    int client_sock;
-    //descriptor socket - IPv4, TCP, default protocol
-    int server_sock = socket(AF_INET, SOCK_STREAM, 0); 
+    // Descriptor socket - IPv4, TCP, default protocol
+    int server_sock = socket(AF_INET, SOCK_STREAM, 0);
     if (server_sock < 0)
-        perror("failed to create socket!\n");
+        printf("failed to create socket! : %d.\n", errno);
         return -1;
     printf("Socket created\n");
 
-    //Keeping address information of both the server and client
-    struct sockaddr_in server_addr, client_addr; 
-    socklen_t client_size;
-    
-    //clean existing data
-    memset(&server_addr, '\0', sizeof(server_addr));  
+    // Keeping address information of both the server and client
+    struct sockaddr_in server_addr, client_addr;
+    int client_size = sizeof(client_addr);
+
+    // clean existing data
+    memset(&server_addr, '\0', sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(SERVER_PORT);
     server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
 
-    //bind the socket to the server address
-    int binder = bind(server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr)); 
+    // bind the socket to the server address
+    if (bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
+    {
+        printf("Bind failed with error code: %d.\n", errno);
+    }
     printf("finished binding!\n");
 
-    //Socket listen to incoming calls
-    int listener = listen(server_sock, 5);  
+    // Socket listen to incoming calls
+    int listener = listen(server_sock, 5);
     printf("Listening...\n");
 
-    client_size = sizeof(client_addr);
-    //Accept incoming calls
-    client_sock = accept(server_sock, (struct sockaddr*)&client_addr, &client_size); 
+
+    // Accept incoming calls
+    int client_sock = accept(server_sock, (struct sockaddr *)&client_addr, &client_size);
     printf("Client connected..!\n");
-    
-    int receiver = recv(client_sock, client_size, strlen(BUFFER_SIZE), 0);
- 
-    close(client_sock);
-    close(server_sock);
+
+    char chunk[1024] = {0};
+    int receiver = recv(client_sock, chunk, sizeof(BUFFER_SIZE), 0);
 
     return 0;
 }
--- Ex3/Sender.c
+++ Ex3/Sender.c
@@ -2,27 +2,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
 #include <string.h>
 #include <arpa/inet.h>
+#include <errno.h>
+#include <signal.h>
 #define CLIENT_PORT 8080
 #define CLIENT_IP "127.0.0.1"
 #define BUFFER_SIZE 1024
 #define CHUNK 1024 // Read 1024 bytes at a time
 
+void sendToReceiver(int socket)
+{
+    char buffer[BUFFER_SIZE] = {'\0'};
+    char buf[CHUNK];
+    // Read 1MB file and sent it to Receiver
+    size_t file_size;
+    FILE *message;
+    message = fopen("test.txt", "r");
+
+    while ((file_size = fread(buf, 1, sizeof(buf), message)) > 0)
+    {
+        int bytesSent = send(socket, buf, CHUNK, 0);
+        if (bytesSent == -1)
+        {
+            printf("send() failed with error code : %d.\n", errno);
+        }
+        else if (bytesSent == 0)
+        {
+            printf("peer has closed the TCP connection prior to send().\n");
+        }
+        else if (bytesSent < file_size)
+        {
+            printf("sent only %ld bytes from the required %d.\n", file_size, bytesSent);
+        }
+        else
+        {
+            printf("message was successfully sent.\n");
+        }
+    }
+    fclose(message);
+}
+
 int main()
 {
-    int client_sock;
     // Create socket - IPv4, TCP, default protocol
     int client_sock = socket(AF_INET, SOCK_STREAM, 0);
     if (client_sock < 0)
-        printf("failed to create socket!\n");
-    return -1;
+    {
+        printf("failed to create socket! : %d.\n", errno);
+        return -1;
+    }
     printf("Socket created\n");
 
     struct sockaddr_in server_addr;
-    socklen_t addr_size;
-
     // clean
     memset(&server_addr, '\0', sizeof(server_addr));
     // configure details
@@ -30,38 +65,20 @@ int main()
     server_addr.sin_port = htons(CLIENT_PORT);
     if (inet_pton(AF_INET, CLIENT_IP, &server_addr.sin_addr) <= 0)
     {
-        perror("Convert was not succeful\n");
+        printf("inet_pton() failed\n");
         return -1;
     }
 
     if (connect(client_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
     {
-        perror("connection failed :(\n");
+        printf("connection failed with error code : %d.\n", errno);
+        close(client_sock);
         return -1;
     };
 
-    char buffer[BUFFER_SIZE] = {'\0'};
-    char buf[CHUNK];
-
-    // Read 1MB file ans sent it to Receiver
-    FILE *message;
-    size_t nread;
-
-    message = fopen("test.txt", "r");
-    if (message)
-    {
-        while ((nread = fread(buf, 1, sizeof(buf), message)) > 0)
-        {
-            send(client_sock, buf, CHUNK, 0);
-        }
-        if (ferror(message))
-        {
-            perror("File is wrong\n");
-            return -1;
-        }
-        fclose(message);
-    }
-
+    printf("Connected to server\n");
+    // Send data away
+    sendToReceiver(client_sock);
     close(client_sock);
 
     return 0;
