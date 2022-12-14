da786b92006d3181db682c9603604666c06048b5
noyrosenbaum <noyrosen123@gmail.com>
2022-12-14 15:16:01 +0200

    Insert all functional methods to Receiver

--- Ex3/Receiver.c
+++ Ex3/Receiver.c
@@ -8,19 +8,19 @@
 int main()
 {
     int client_sock;
-    int server_sock = socket(AF_INET, SOCK_STREAM, 0);
+    char buffer[1024];
+    int server_sock = socket(AF_INET, SOCK_STREAM, 0); //descriptor socket
     if (server_sock < 0)
+        printf("failed to create socket!\n");
         return -1;
     printf("Socket created\n");
 
-    ////////////////////////////////
     struct sockaddr_in server_addr, client_addr; //Keeping address information of both the server and client
     socklen_t client_size;
-    char buffer[1024];
-
-    memset(&server_addr, '\0', sizeof(server_addr)); //Initialize server address 
+    
+    memset(&server_addr, '\0', sizeof(server_addr)); //clean existing data 
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(2000);
+    server_addr.sin_port = htons(SERVER_PORT);
     server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
 
     int binder = bind(server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr)); //bind the socket to the server address
@@ -33,19 +33,10 @@ int main()
     client_sock = accept(server_sock, (struct sockaddr*)&client_addr, &client_size); //Accept incoming calls
     printf("Client connected..!\n");
     
-    int receiver = recv(client_sock, buffer);
+    int receiver = recv(client_sock, client_size, strlen(buffer), 0);
  
-
-
-    // int yes = 1;
-    // If(setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof yes) == -1)
-    // {
-    //     perror("setsockopt");
-    //     exit(1);
-    // }
+    close(client_sock);
+    close(server_sock);
 
     return 0;
 }
-
-
-/////////////////////
