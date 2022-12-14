c481f20fd59d5e566ae4da0b63811b318fd77606
noyrosenbaum <noyrosen123@gmail.com>
2022-12-14 18:47:48 +0200

    Change the position of comments

--- Ex3/Receiver.c
+++ Ex3/Receiver.c
@@ -4,36 +4,42 @@
 #include <arpa/inet.h>
 #define SERVER_PORT 8080
 #define SERVER_IP "127.0.0.1"
+#define BUFFER_SIZE 1024
 
 int main()
 {
     int client_sock;
-    char buffer[1024];
-    int server_sock = socket(AF_INET, SOCK_STREAM, 0); //descriptor socket - IPv4, TCP, default protocol
+    //descriptor socket - IPv4, TCP, default protocol
+    int server_sock = socket(AF_INET, SOCK_STREAM, 0); 
     if (server_sock < 0)
         perror("failed to create socket!\n");
         return -1;
     printf("Socket created\n");
 
-    struct sockaddr_in server_addr, client_addr; //Keeping address information of both the server and client
+    //Keeping address information of both the server and client
+    struct sockaddr_in server_addr, client_addr; 
     socklen_t client_size;
     
-    memset(&server_addr, '\0', sizeof(server_addr)); //clean existing data 
+    //clean existing data
+    memset(&server_addr, '\0', sizeof(server_addr));  
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(SERVER_PORT);
     server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
 
-    int binder = bind(server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr)); //bind the socket to the server address
+    //bind the socket to the server address
+    int binder = bind(server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr)); 
     printf("finished binding!\n");
 
-    int listener = listen(server_sock, 5); //Socket listen to incoming calls 
+    //Socket listen to incoming calls
+    int listener = listen(server_sock, 5);  
     printf("Listening...\n");
 
     client_size = sizeof(client_addr);
-    client_sock = accept(server_sock, (struct sockaddr*)&client_addr, &client_size); //Accept incoming calls
+    //Accept incoming calls
+    client_sock = accept(server_sock, (struct sockaddr*)&client_addr, &client_size); 
     printf("Client connected..!\n");
     
-    int receiver = recv(client_sock, client_size, strlen(buffer), 0);
+    int receiver = recv(client_sock, client_size, strlen(BUFFER_SIZE), 0);
  
     close(client_sock);
     close(server_sock);
