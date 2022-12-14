780e6ee3220b92395431bcee18b23767e3707bcc
noyrosenbaum <noyrosen123@gmail.com>
2022-12-14 19:36:20 +0200

    Send file functionality

--- Ex3/Sender.c
+++ Ex3/Sender.c
@@ -8,21 +8,13 @@
 #define CLIENT_PORT 8080
 #define CLIENT_IP "127.0.0.1"
 #define BUFFER_SIZE 1024
+#define CHUNK 1024 // Read 1024 bytes at a time
 
 int main()
 {
-    // Read 1MB file
-    FILE *message = fopen("test.txt", "r");
-    if (message == NULL)
-    {
-        perror("File is Empty\n");
-        return -1;
-    }
-
-
     int client_sock;
     // Create socket - IPv4, TCP, default protocol
-    int client_sock = socket(AF_INET, SOCK_STREAM, 0); 
+    int client_sock = socket(AF_INET, SOCK_STREAM, 0);
     if (client_sock < 0)
         printf("failed to create socket!\n");
     return -1;
@@ -33,10 +25,10 @@ int main()
 
     // clean
     memset(&server_addr, '\0', sizeof(server_addr));
-    // configure details 
-    server_addr.sin_family = AF_INET;                
+    // configure details
+    server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(CLIENT_PORT);
-    if (inet_pton(AF_INET, CLIENT_IP, &server_addr.sin_addr) <= 0) 
+    if (inet_pton(AF_INET, CLIENT_IP, &server_addr.sin_addr) <= 0)
     {
         perror("Convert was not succeful\n");
         return -1;
@@ -47,9 +39,30 @@ int main()
         perror("connection failed :(\n");
         return -1;
     };
-    // sent()
+
+    char buffer[BUFFER_SIZE] = {'\0'};
+    char buf[CHUNK];
+
+    // Read 1MB file ans sent it to Receiver
+    FILE *message;
+    size_t nread;
+
+    message = fopen("test.txt", "r");
+    if (message)
+    {
+        while ((nread = fread(buf, 1, sizeof(buf), message)) > 0)
+        {
+            send(client_sock, buf, CHUNK, 0);
+        }
+        if (ferror(message))
+        {
+            perror("File is wrong\n");
+            return -1;
+        }
+        fclose(message);
+    }
 
     close(client_sock);
-    fclose(message);
+
     return 0;
 }
