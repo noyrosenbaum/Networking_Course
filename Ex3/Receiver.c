d160bc2cc109cce44e86b25d262a671b6d3be07e
noyrosenbaum <noyrosen123@gmail.com>
2022-12-14 14:45:27 +0200

    Modify Receiver, Create makefile

--- Ex3/Receiver.c
+++ Ex3/Receiver.c
@@ -1,26 +1,51 @@
 #include <sys/types.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <sys/socket.h>
 #include <string.h>
 #include <arpa/inet.h>
-#define CLIENT_PORT 8999
-#define CLIENT_IP "127.0.0.1"
+#define SERVER_PORT 8080
+#define SERVER_IP "127.0.0.1"
 
 int main()
 {
-    // int sock = socket(AF_INET, SOCK_STREAM, 0); // IPv4, TCP connection, IP
-    //  int connectResult = connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr));
-    // if (connectResult == -1) {
-    //     return -1;
-    // }
+    int client_sock;
+    int server_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_sock < 0)
+        return -1;
+    printf("Socket created\n");
 
-    // // Set port and IP of server
-    // struct sockaddr_in server_addr;
-    // server_addr.sin_family = AF_INET;
-    // server_addr.sin_port = htons(CLIENT_PORT);
-    // server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    ////////////////////////////////
+    struct sockaddr_in server_addr, client_addr; //Keeping address information of both the server and client
+    socklen_t client_size;
+    char buffer[1024];
+
+    memset(&server_addr, '\0', sizeof(server_addr)); //Initialize server address 
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(2000);
+    server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+
+    int binder = bind(server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr)); //bind the socket to the server address
+    printf("finished binding!\n");
+
+    int listener = listen(server_sock, 5); //Socket listen to incoming calls 
+    printf("Listening...\n");
+
+    client_size = sizeof(client_addr);
+    client_sock = accept(server_sock, (struct sockaddr*)&client_addr, &client_size); //Accept incoming calls
+    printf("Client connected..!\n");
+    
+    int receiver = recv(client_sock, buffer);
+ 
+
+
+    // int yes = 1;
+    // If(setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof yes) == -1)
+    // {
+    //     perror("setsockopt");
+    //     exit(1);
+    // }
 
     return 0;
 }
+
+
+/////////////////////
--- Ex3/Sender.c
+++ Ex3/Sender.c
@@ -1,47 +1,31 @@
 #include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <sys/socket.h>
 #include <string.h>
 #include <arpa/inet.h>
-#define SERVER_PORT 8080
-#define SERVER_IP "127.0.0.1"
+#define CLIENT_PORT 8999
+#define CLIENT_IP "127.0.0.1"
 
 int main()
 {
     int client_sock;
-    int server_sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_sock < 0)
+    int client_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_sock < 0)
         return -1;
     printf("Socket created\n");
 
-    struct sockaddr_in server_addr, client_addr;
+    struct sockaddr_in addr;
     socklen_t addr_size;
     char buffer[1024];
 
-    memset(&server_addr, "\0", sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = SERVER_PORT;
-    server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+    memset(&addr, "\0", sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = CLIENT_PORT;
+    addr.sin_addr.s_addr = inet_addr(CLIENT_IP);
 
-    int binder = bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr));
-    int listener = listen(server_sock, 5);
-    printf("Listening...\n");
-
-    while (1)
-    {
-        addr_size = sizeof(client_addr);
-        client_sock = accept(server_sock, (struct sockaddr*)&client_addr, &addr_size);
-        printf("Client connected..!\n");
-    }
-    
-
-
-
-    // int yes = 1;
-    // If(setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof yes) == -1)
-    // {
-    //     perror("setsockopt");
-    //     exit(1);
-    // }
-
+    connect(client_sock, (struct sockaddr*)&addr, sizeof(addr));
+    printf("Connected to the server");
     return 0;
 }
