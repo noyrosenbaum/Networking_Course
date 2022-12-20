cedc42f3c82c789cd664946f5f9a01e438d467fd
noyrosenbaum <noyrosen123@gmail.com>
2022-12-20 12:33:16 +0200

    Establish Server socket

--- Ex3/ReceiverDraft.c
+++ Ex3/ReceiverDraft.c
@@ -1,1 +1,103 @@
-//hi
+// Linux and other UNIXes
+#include <stdio.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define SERVER_PORT 5060 // The port that the server listens
+#define BUFFER_SIZE 1024
+#define CHUNK 1024 // Read 1024 bytes at a time
+#define FILE_SIZE 1060424
+static double totalTimes = 0;
+
+int createSocket(struct sockaddr_in *serverAddress)
+{
+    // Open the listening (server) socket
+    int listeningSocket = -1;
+    listeningSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); // 0 means default protocol for stream sockets (Equivalently, IPPROTO_TCP)
+    if (listeningSocket == -1)
+    {
+        printf("Could not create listening socket : %d", errno);
+        return 1;
+    }
+
+    // Reuse the address if the server socket on was closed
+    // and remains for 45 seconds in TIME-WAIT state till the final removal.
+    //
+    int enableReuse = 1;
+    int ret = setsockopt(listeningSocket, SOL_SOCKET, SO_REUSEADDR, &enableReuse, sizeof(int));
+    if (ret < 0)
+    {
+        printf("setsockopt() failed with error code : %d", errno);
+        return 1;
+    }
+
+    // "sockaddr_in" is the "derived" from sockaddr structure
+    // used for IPv4 communication. For IPv6, use sockaddr_in6
+    //
+    struct sockaddr_in serverAddress;
+    memset(&serverAddress, 0, sizeof(serverAddress));
+
+    serverAddress->sin_family = AF_INET;
+    serverAddress->sin_addr.s_addr = INADDR_ANY;  // any IP at this port (Address to accept any incoming messages)
+    serverAddress->sin_port = htons(SERVER_PORT); // network order (makes byte order consistent)
+
+    // Bind the socket to the port with any IP at this port
+    int bindResult = bind(listeningSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
+    if (bindResult == -1)
+    {
+        printf("Bind failed with error code : %d", errno);
+        // close the socket
+        close(listeningSocket);
+        return -1;
+    }
+
+    printf("Bind() success\n");
+
+    // Make the socket listening; actually mother of all client sockets.
+    // 500 is a Maximum size of queue connection requests
+    // number of concurrent connections
+    int listenResult = listen(listeningSocket, 3);
+    if (listenResult == -1)
+    {
+        printf("listen() failed with error code : %d", errno);
+        // close the socket
+        close(listeningSocket);
+        return -1;
+    }
+    printf("Sender's socket successfully created\n");
+    return listeningSocket;
+}
+
+int main()
+{
+    // Create Client socket
+    int serverSocket;
+    struct sockaddr_in serverAddress;
+    serverSocket = createSocket(&serverAddress);
+    printf("Listen to client\n");
+    // Accept and incoming connection
+    printf("Waiting for incoming TCP-connections...\n");
+    struct sockaddr_in clientAddress;
+    socklen_t clientAddressLen = sizeof(clientAddress);
+    memset(&clientAddress, 0, sizeof(clientAddress));
+    clientAddressLen = sizeof(clientAddress);
+    // Accepts incoming connections
+    int clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddress, &clientAddressLen);
+    if (clientSocket == -1)
+    {
+        printf("listen failed with error code : %d", errno);
+        // close the sockets
+        close(serverSocket);
+        return -1;
+    }
+
+    printf("A new client connection accepted\n");
+    return 0;
+}
