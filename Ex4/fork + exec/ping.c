92e02fd0301148b70e420a7fea81988f14db6a4b
noyrosenbaum <noyrosen123@gmail.com>
2022-12-28 13:45:55 +0200

    Initialize project

---
+++ Ex4/fork + exec/ping.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/time.h> // gettimeofday()
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <resolv.h>
+#include <netdb.h>
+#include <sys/wait.h>
+
+#define PACKETSIZE 64
+#define SOURCE_IP "127.0.0.1"
+#define DESTINATION_IP "127.0.0.1"
+struct packet
+{
+    struct icmphdr hdr;
+    char msg[PACKETSIZE - sizeof(struct icmphdr)];
+};
+
+// int pid = -1;
+// int loops = 25;
+// struct protoent *proto = NULL;
+
+int main()
+{
+    return 0;
+}
+
+unsigned short checksum(void *b, int len)
+{
+    unsigned short *buf = b;
+    unsigned int sum = 0;
+    unsigned short result;
+    for (sum = 0; len > 1; len -= 2)
+        sum += *buf++;
+    if (len == 1)
+        sum += *(unsigned char *)buf;
+    sum = (sum >> 16) + (sum & 0xFFFF);
+    sum += (sum >> 16);
+    result = ~sum;
+    return result;
+}
