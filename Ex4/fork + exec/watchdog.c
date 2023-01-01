3451264507ccc178de1d3d9ed3848b8eb3a690ce
noyrosenbaum <noyrosen123@gmail.com>
2023-01-01 21:38:05 +0200

    Set a TIMEOUT variable in watchdog

--- Ex4/fork + exec/watchdog.c
+++ Ex4/fork + exec/watchdog.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+#include <sys/time.h>
+
+#define TIMEOUT 10 // milliseconds - ENSURE IN CODE THE TIME UNITS
 
 int main()
 {
+    int timer = 0;
     printf("hello partb");
-    while (timer < 10seconds)
+    while (timer < 10)
     {
         recv();
         timer = 0seconds;
