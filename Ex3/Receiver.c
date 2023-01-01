eb0c79ec84f5bac889e1cfa12a5305488499fac8
noyrosenbaum <noyrosen123@gmail.com>
2023-01-01 22:18:27 +0200

    Update working code in Github

--- Ex3/Receiver.c
+++ Ex3/Receiver.c
@@ -1,55 +1,232 @@
-// #include <sys/types.h>
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <unistd.h>
-// #include <sys/types.h>
-// #include <sys/socket.h>
-// #include <netinet/in.h>
-// #include <string.h>
-// #include <arpa/inet.h>
-// #include <errno.h>
-// #include <signal.h>
-// #define SERVER_PORT 8080
-// #define SERVER_IP "127.0.0.1"
-// #define BUFFER_SIZE 1024
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/time.h>
 
-// int main()
-// {
-//     // Descriptor socket - IPv4, TCP, default protocol
-//     int server_sock = socket(AF_INET, SOCK_STREAM, 0);
-//     if (server_sock < 0)
-//         printf("failed to create socket! : %d.\n", errno);
-//         return -1;
-//     printf("Socket created\n");
+#define RECEIVER_PORT 9999 // The port that the receiver listens
+#define BUFFER_SIZE 35342
+#define FILE_SIZE 1060424
+char buffer[BUFFER_SIZE];
+int bytesReceived = 0;
+int bytesReceived2 = 0;
+char exitMessage[5] = {0};
+char typeofcc[6] = "cubic";
+double elapsedCubic = 0;
+double elapsedReno = 0;
+int i = 1;
+double avaragepartone = 0;
+double avarageparttwo = 0;
+int userResponse = 1;
+double timesofpartone[1000];
+double timesofparttwo[1000];
+long seconds;
+long microsec;
 
-//     // Keeping address information of both the server and client
-//     struct sockaddr_in server_addr, client_addr;
-//     int client_size = sizeof(client_addr);
+int main()
+{
 
-//     // clean existing data
-//     memset(&server_addr, '\0', sizeof(server_addr));
-//     server_addr.sin_family = AF_INET;
-//     server_addr.sin_port = htons(SERVER_PORT);
-//     server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+    // Create listening socket
+    int listeningSocket;
+    struct sockaddr_in receiverAddress;
 
-//     // bind the socket to the server address
-//     if (bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
-//     {
-//         printf("Bind failed with error code: %d.\n", errno);
-//     }
-//     printf("finished binding!\n");
+    listeningSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); // 0 means default protocol for stream sockets (Equivalently, IPPROTO_TCP)
 
-//     // Socket listen to incoming calls
-//     int listener = listen(server_sock, 5);
-//     printf("Listening...\n");
+    if (listeningSocket == -1)
+    {
+        printf("Could not create listening socket : %d\n", errno);
+        return -1;
+    }
+    else
+        (printf("A new listening socket has been created\n"));
 
+    // Reuse the address if the server socket on was closed
+    // and remains for 45 seconds in TIME-WAIT state till the final removal.
+    int enableReuse = 1;
+    int ret = setsockopt(listeningSocket, SOL_SOCKET, SO_REUSEADDR, &enableReuse, sizeof(int));
+    if (ret < 0)
+    {
+        printf("Setsockopt() failed with error code : %d\n", errno);
+        return 1;
+    }
 
-//     // Accept incoming calls
-//     int client_sock = accept(server_sock, (struct sockaddr *)&client_addr, &client_size);
-//     printf("Client connected..!\n");
+    // "sockaddr_in" is the "derived" from sockaddr structure
+    // used for IPv4 communication. For IPv6, use sockaddr_in6
+    //
 
-//     char chunk[1024] = {0};
-//     int receiver = recv(client_sock, chunk, sizeof(BUFFER_SIZE), 0);
+    memset(&receiverAddress, 0, sizeof(receiverAddress));
 
-//     return 0;
-// }
+    receiverAddress.sin_family = AF_INET;
+    receiverAddress.sin_addr.s_addr = INADDR_ANY;    // any IP at this port (Address to accept any incoming messages)
+    receiverAddress.sin_port = htons(RECEIVER_PORT); // network order (makes byte order consistent)
+
+    // Bind the socket to the port with any IP at this port
+    int bindResult = bind(listeningSocket, (struct sockaddr *)&receiverAddress, sizeof(receiverAddress));
+    if (bindResult == -1)
+    {
+        printf("Bind failed with error code : %d\n", errno);
+        // close the socket
+        close(listeningSocket);
+        exit(1);
+    }
+
+    printf("Bind() success\n");
+
+    // Make the socket listening; actually mother of all client sockets.
+    // 500 is a Maximum size of queue connection requests
+    // number of concurrent connections
+    int listenResult = listen(listeningSocket, 4);
+    if (listenResult == -1)
+    {
+        printf("Listen() failed with error code : %d\n", errno);
+        // close the socket
+        close(listeningSocket);
+        return -1;
+    }
+    printf("Listening to sender\n");
+
+    // Build receiver information
+    printf("Waiting for incoming TCP-connections...\n");
+    struct sockaddr_in senderAddress;
+    socklen_t senderAddressLen = sizeof(senderAddress);
+    memset(&senderAddress, 0, sizeof(senderAddress));
+    senderAddressLen = sizeof(senderAddress);
+
+    // Accepts incoming connections
+    int senderSocket = accept(listeningSocket, (struct sockaddr *)&senderAddress, &senderAddressLen);
+    if (senderSocket == -1)
+    {
+        printf("Listen failed with error code : %d\n", errno);
+        // close the sockets
+        close(listeningSocket);
+        return -1;
+    }
+
+    printf("A new connection accepted\n");
+
+    struct timeval beginCubic, endCubic;
+
+    while (userResponse)
+    {
+        gettimeofday(&beginCubic, 0);
+        long total = 0;
+        while (total != (FILE_SIZE / 2))
+        {
+            bytesReceived = recv(senderSocket, buffer, BUFFER_SIZE, 0);
+            total += bytesReceived;
+            if (bytesReceived == -1)
+            {
+                printf("Recv failed with error code : %d\n", errno);
+                close(senderSocket);
+                return -1;
+            }
+            if (bytesReceived == 0)
+            {
+                printf("Connection socket closed\n");
+                userResponse = 0;
+                break;
+            }
+        }
+
+        if (!userResponse)
+            break;
+
+        printf("The first file has been recevied\n");
+        gettimeofday(&endCubic, 0);
+        seconds = endCubic.tv_sec - beginCubic.tv_sec;
+        microsec = endCubic.tv_usec - beginCubic.tv_usec;
+        elapsedCubic = seconds + microsec *1e-6;
+        timesofpartone[i - 1] = elapsedCubic;
+
+        // Send authentication message to sender
+        // authentication message - bitwise XOR of last 4 digits of our IDs
+        char authentication[] = "10000010111111";
+        send(senderSocket, &authentication, sizeof(authentication), 0);
+        printf("Sending ACK to send\n");
+
+        // change algorithm to reno
+        printf("Change to reno method\n");
+        strcpy(typeofcc, "reno");
+        if (setsockopt(senderSocket, IPPROTO_TCP, TCP_CONGESTION, typeofcc, sizeof(typeofcc)) != 0)
+        {
+            perror("Setsockopt error");
+            return -1;
+        }
+
+        // receiving second part
+        struct timeval beginReno, endReno;
+        gettimeofday(&beginReno, 0);
+        total = 0;
+        while (total != (FILE_SIZE / 2))
+        {
+            bytesReceived = recv(senderSocket, buffer, BUFFER_SIZE, 0);
+            total += bytesReceived;
+            if (bytesReceived == -1)
+            {
+                printf("Recv failed with error code : %d\n", errno);
+                close(senderSocket);
+                return -1;
+            }
+            if (bytesReceived == 0)
+            {
+                printf("Connection socket closed\n");
+                userResponse = 0;
+                break;
+            }
+        }
+
+        if (!userResponse)
+            break;
+
+        printf("The second file has been received\n");
+        gettimeofday(&endReno, 0);
+
+        seconds = endReno.tv_sec - beginReno.tv_sec;
+        microsec = endReno.tv_usec - beginReno.tv_usec;
+        elapsedReno = seconds + microsec *1e-6;
+        timesofparttwo[i - 1] = elapsedReno;
+
+        char dummyvar = '\0';
+
+        send(senderSocket,&dummyvar,sizeof(char),0);
+
+        printf("Receiver waiting for sender decision\n");
+
+        recv(senderSocket,&dummyvar,sizeof(char),0);
+        send(senderSocket,&dummyvar,sizeof(char),0);
+        i++;
+
+    }
+
+    close(senderSocket);
+    close(listeningSocket);
+
+    int j = 0;
+
+    while (j+1 < i){
+        printf("The first part of receving massage at iteration number %d took %f seconds\n", j + 1, timesofpartone[j]);
+        avaragepartone += timesofpartone[j];
+        printf("The second part of receving massage at iteration number %d took %f seconds\n", j + 1, timesofparttwo[j]);
+        avarageparttwo += timesofparttwo[j];
+        j++;
+    }
+    avaragepartone = avaragepartone / --i;
+    avarageparttwo = avarageparttwo / i;
+    if (i==1)
+        printf("There was only one iteration. no avarage time to print\n");
+    else{
+    printf("The avarge time of receiving part one is: %f\n", avaragepartone);
+    printf("The avarge time of receiving part two is: %f\n", avarageparttwo);}
+    
+    printf("The total avarage time for receiving file is: %f\n", (avaragepartone+avarageparttwo)/2);
+    printf("Bye bye:)\n");
+
+    return 0;
+}
--- Ex3/Sender.c
+++ Ex3/Sender.c
@@ -1,85 +1,177 @@
-// #include <sys/types.h>
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <unistd.h>
-// #include <sys/types.h>
-// #include <sys/socket.h>
-// #include <netinet/in.h>
-// #include <string.h>
-// #include <arpa/inet.h>
-// #include <errno.h>
-// #include <signal.h>
-// #define CLIENT_PORT 8080
-// #define CLIENT_IP "127.0.0.1"
-// #define BUFFER_SIZE 1024
-// #define CHUNK 1024 // Read 1024 bytes at a time
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <signal.h>
 
-// void sendToReceiver(int socket)
-// {
-//     char buffer[BUFFER_SIZE] = {'\0'};
-//     char buf[CHUNK];
-//     // Read 1MB file and sent it to Receiver
-//     size_t file_size;
-//     FILE *message;
-//     message = fopen("test.txt", "r");
+#define RECEIVER_PORT 9999
+#define RECEIVER_IP_ADDRESS "127.0.0.1"
+#define BUFFER_SIZE 35342
+#define FILE_SIZE 1060424
 
-//     while ((file_size = fread(buf, 1, sizeof(buf), message)) > 0)
-//     {
-//         int bytesSent = send(socket, buf, CHUNK, 0);
-//         if (bytesSent == -1)
-//         {
-//             printf("send() failed with error code : %d.\n", errno);
-//         }
-//         else if (bytesSent == 0)
-//         {
-//             printf("peer has closed the TCP connection prior to send().\n");
-//         }
-//         else if (bytesSent < file_size)
-//         {
-//             printf("sent only %ld bytes from the required %d.\n", file_size, bytesSent);
-//         }
-//         else
-//         {
-//             printf("message was successfully sent.\n");
-//         }
-//     }
-//     fclose(message);
-// }
+char firstPart[(FILE_SIZE / 2)];
+char secondPart[(FILE_SIZE / 2)];
+char exitMessage[5] = "Exit";
+char authentication[] = "10000010111111"; // bitwise XOR between both IDs
+char typeofcc[6] = "cubic";
+int byterec = 0;
+char answer[4];
+char confirm[4];
+int byteSend = 0;
 
-// int main()
-// {
-//     // Create socket - IPv4, TCP, default protocol
-//     int client_sock = socket(AF_INET, SOCK_STREAM, 0);
-//     if (client_sock < 0)
-//     {
-//         printf("failed to create socket! : %d.\n", errno);
-//         return -1;
-//     }
-//     printf("Socket created\n");
+int main()
+{
 
-//     struct sockaddr_in server_addr;
-//     // clean
-//     memset(&server_addr, '\0', sizeof(server_addr));
-//     // configure details
-//     server_addr.sin_family = AF_INET;
-//     server_addr.sin_port = htons(CLIENT_PORT);
-//     if (inet_pton(AF_INET, CLIENT_IP, &server_addr.sin_addr) <= 0)
-//     {
-//         printf("inet_pton() failed\n");
-//         return -1;
-//     }
+    //  Read 1MB file
+    FILE *message;
+    message = fopen("test.txt", "r");
+    fread(firstPart, 1, sizeof(firstPart), message);   // read the first part of file
+    fread(secondPart, 1, sizeof(secondPart), message); // read the second part
+    fclose(message);
 
-//     if (connect(client_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
-//     {
-//         printf("connection failed with error code : %d.\n", errno);
-//         close(client_sock);
-//         return -1;
-//     };
+    // Create a sender socket
+    int senderSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); // 0 means default protocol for stream sockets (Equivalently, IPPROTO_TCP)
+    if (senderSocket == -1)
+    {
+        printf("Could not create socket : %d\n", errno);
+        return -1;
+    }
+    struct sockaddr_in receiverAddress;
+    memset(&receiverAddress, 0, sizeof(receiverAddress));
+    receiverAddress.sin_family = AF_INET;                                                          // version ipv4
+    receiverAddress.sin_port = htons(RECEIVER_PORT);                                               // (5001 = 0x89 0x13) little endian => (0x13 0x89) network endian (big endian)
+    int rval = inet_pton(AF_INET, (const char *)RECEIVER_IP_ADDRESS, &(receiverAddress.sin_addr)); // convert IPv4 and IPv6 addresses from text to binary form
+    // e.g. 127.0.0.1 => 0x7f000001 => 01111111.00000000.00000000.00000001 => 2130706433
+    if (rval <= 0)
+    {
+        printf("inet_pton() failed\n");
+        return -1;
+    }
+    printf("Socket successfully created\n");
 
-//     printf("Connected to server\n");
-//     // Send data away
-//     sendToReceiver(client_sock);
-//     close(client_sock);
+    // Make a connection to the receiver with socket SendingSocket
+    int connectResult = connect(senderSocket, (struct sockaddr *)&receiverAddress, sizeof(receiverAddress));
+    if (connectResult == -1)
+    {
+        printf("Connect() failed with error code : %d\n", errno);
+        // cleanup the socket;
+        close(senderSocket);
+        return -1;
+    }
 
-//     return 0;
-// }
+    printf("Connected to receiver\n");
+
+    // while the answer is yes, run this while loop
+    while (1)
+    {
+        byteSend = send(senderSocket, firstPart, (FILE_SIZE / 2), 0);
+        if (byteSend == -1)
+        {
+            printf("Send() failed with error code : %d\n", errno);
+            close(senderSocket);
+            return -1;
+        }
+
+        else if (byteSend == 0)
+        {
+            printf("Peer has closed the TCP connection prior to send().\n");
+            return -1;
+        }
+
+        else
+        {
+            printf("First part was successfully sent %d bytes.\n", byteSend);
+        }
+
+        printf("Waiting for ACK\n");
+
+        char authenticationRep[16] = {0};
+
+        byterec = recv(senderSocket, authenticationRep, sizeof(authenticationRep), 0);
+        if (!strcmp(authenticationRep, authentication))
+        {
+            printf("Authentication Approved\n");
+        }
+        if (byterec == 0)
+        {
+            printf("Peer has closed the TCP connection prior to send().\n");
+            close(senderSocket);
+            return -1;
+        }
+        else if (byterec == -1)
+        {
+            printf("recv() failed with error code : %d\n", errno);
+            close(senderSocket);
+            return -1;
+        }
+
+        // change to reno algorithm
+        printf("Change to reno method\n");
+        strcpy(typeofcc, "reno");
+        if (setsockopt(senderSocket, IPPROTO_TCP, TCP_CONGESTION, typeofcc, sizeof(typeofcc)) != 0)
+        {
+            perror("Setsockopt error");
+            return -1;
+        }
+
+        // sending second part of file
+        printf("Sending the second part of the file\n");
+        int bytesSend = send(senderSocket, secondPart, (FILE_SIZE / 2), 0);
+        if (bytesSend == -1)
+        {
+            printf("Send() failed with error code : %d\n", errno);
+            close(senderSocket);
+            return -1;
+        }
+        else if (bytesSend == 0)
+        {
+            printf("Peer has closed the TCP connection prior to send().\n");
+        }
+        else
+        {
+            printf("Second part was successfully sent %d bytes\n", byteSend);
+        }
+
+        char dummyvar = '\0';
+        recv(senderSocket, &dummyvar, sizeof(char), 0);
+
+        // checking user preferences
+        printf("Send the file again? Write 'yes' for sending again or 'Exit' for finish session.\n");
+        scanf(" %s", answer);
+
+        send(senderSocket, &dummyvar, sizeof(char), 0);
+        recv(senderSocket, &dummyvar, sizeof(char), 0);
+        printf("Your answer is: %s\n", answer);
+
+        if (!strcmp(answer, "yes"))
+        {
+            printf("Running this process once again\n");
+            // change algorithm if user's answer is yes and sends file once again
+            byterec = 0;
+            printf("Change to cubic method\n");
+            strcpy(typeofcc, "cubic");
+            if (setsockopt(senderSocket, IPPROTO_TCP, TCP_CONGESTION, typeofcc, sizeof(typeofcc)) != 0)
+            {
+                perror("setsockopt");
+                return -1;
+            }
+        }
+
+        else
+        {
+            printf("Session is over\n");
+            break;
+        }
+    }
+
+    // close socket
+    close(senderSocket);
+
+    return 0;
+}
