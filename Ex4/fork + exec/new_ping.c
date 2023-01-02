e145ddf9fd3befad2574c118b9aca850ad89d733
noyrosenbaum <noyrosen123@gmail.com>
2023-01-02 13:05:15 +0200

    Add TCP socket

--- Ex4/fork + exec/new_ping.c
+++ Ex4/fork + exec/new_ping.c
@@ -13,6 +13,9 @@
 #include <signal.h>
 #include <fcntl.h>
 
+#define SERVER_PORT 3000
+#define IP "127.0.0.1"
+
 // IPv4 header len without options
 #define IP4_HDRLEN 20
 
@@ -24,6 +27,31 @@ int sock = -1;
 
 // Checksum algo
 unsigned short calculate_checksum(unsigned short *paddress, int len);
+
+// Craete new socket
+int createSocket(struct sockaddr_in *serverAddress)
+{
+    int client_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (client_sock == -1)
+    {
+        printf("Could not create socket : %d\n", errno);
+        return -1;
+    }
+
+    memset(serverAddress, 0, sizeof(*serverAddress));
+
+    serverAddress->sin_family = AF_INET;
+    serverAddress->sin_port = htons(SERVER_PORT);                              // (5001 = 0x89 0x13) little endian => (0x13 0x89) network endian (big endian)
+    int rval = inet_pton(AF_INET, (const char *)IP, &serverAddress->sin_addr); // convert IPv4 and IPv6 addresses from text to binary form
+    // e.g. 127.0.0.1 => 0x7f000001 => 01111111.00000000.00000000.00000001 => 2130706433
+    if (rval <= 0)
+    {
+        printf("inet_pton() failed\n");
+        return -1;
+    }
+    printf("Sender's socket successfully created\n");
+    return client_sock;
+}
 
 // START FROM HERE!!!!!!!!!!!!!!!1
 //  run 2 programs using fork + exec
@@ -71,6 +99,23 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
+    // Create TCP socket
+    int clientSocket;
+    struct sockaddr_in serverAddress;
+    clientSocket = createSocket(&serverAddress);
+
+    // Make a connection to the server with socket SendingSocket.
+    int connectResult = connect(clientSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
+    if (connectResult == -1)
+    {
+        printf("connect() failed with error code : %d\n", errno);
+        // cleanup the socket;
+        close(clientSocket);
+        return -1;
+    }
+
+    printf("connected to server\n");
+
     // compiled watchdog.c by makefile
     args[0] = "./watchdog";
     args[1] = NULL;
@@ -88,7 +133,6 @@ int main(int argc, char *argv[])
     // here we enter ping code
     return 0;
 }
-
 
 // Compute checksum (RFC 1071).
 unsigned short calculate_checksum(unsigned short *paddress, int len)
