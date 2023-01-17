717338cf662b98f485282a8b6e459492e100fd02
noyrosenbaum <noyrosen123@gmail.com>
2023-01-17 21:09:22 +0200

    Add src and dest IP insertion

--- Ex5/Spoofer.c
+++ Ex5/Spoofer.c
@@ -14,8 +14,9 @@
 #include <netinet/udp.h>     // UDP header details
 #include <netinet/ip.h>      // IP header details
 #include <netinet/ip_icmp.h> // ICMP header details
-
-void send_raw_ip_packet(struct iphdr *ip);
+char *newSourceIP;
+char *destinationIP;
+int send_raw_ip_packet(struct iphdr *ip);
 
 unsigned short in_cksum(unsigned short *buf, int length)
 {
@@ -48,10 +49,12 @@ unsigned short in_cksum(unsigned short *buf, int length)
     return (unsigned short)(~sum);
 }
 
-int main()
+int main(int argc, char *argv[])
 {
-    char buffer[1500];
-    memset(buffer, 0, 1500);
+    destinationIP = argv[1];
+    newSourceIP = argv[2];
+    char buffer[IP_MAXPACKET];
+    memset(buffer, 0, IP_MAXPACKET);
     // Ethernet
     // struct ethhdr *ethernet = (struct ethhdr *)buffer;
 
@@ -61,7 +64,7 @@ int main()
     // ICMP
     struct icmphdr *icmp = (struct icmphdr *)(buffer + sizeof(struct iphdr));
 
-    icmp->type = 8; // ICMP Type: 8 is request, 0 is reply.
+    icmp->type = ICMP_ECHO; // ICMP Type: 8 is request, 0 is reply.
 
     // Calculate the checksum for integrity
     icmp->checksum = 0;
@@ -76,8 +79,8 @@ int main()
     ip->version = 4;
     ip->ihl = 5;
     ip->ttl = 20;
-    ip->saddr = inet_addr("1.2.3.4");
-    ip->daddr = inet_addr("10.0.2.5");
+    ip->saddr = inet_addr(newSourceIP);
+    ip->daddr = inet_addr(destinationIP);
     ip->protocol = IPPROTO_ICMP;
     ip->tot_len = htons(sizeof(struct iphdr) +
                         sizeof(struct icmphdr));
@@ -90,24 +93,38 @@ int main()
     return 0;
 }
 
-void send_raw_ip_packet(struct iphdr *ip)
+int send_raw_ip_packet(struct iphdr *ip)
 {
     struct sockaddr_in dest_info;
     int enable = 1;
 
     // Step 1: Create a raw network socket.
     int sock = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
+    if (sock == -1)
+    {
+        fprintf(stderr, "socket() failed with error: %d", errno);
+        fprintf(stderr, "To create a raw socket, the process needs to be run by Admin/root user.\n\n");
+        exit(1);
+    }
 
     // Step 2: Set socket option.
-    setsockopt(sock, IPPROTO_IP, IP_HDRINCL,
-               &enable, sizeof(enable));
+    if (setsockopt(sock, IPPROTO_IP, IP_HDRINCL, &enable, sizeof(enable)) < 0)
+    {
+        printf("setsockopt() failed with error %d\n", errno);
+        exit(1);
+    }
 
     // Step 3: Provide needed information about destination.
+    memset(&dest_info, 0, sizeof(struct sockaddr_in));
     dest_info.sin_family = AF_INET;
     dest_info.sin_addr.s_addr = ip->daddr;
 
     // Step 4: Send the packet out.
-    sendto(sock, ip, ntohs(ip->tot_len), 0,
-           (struct sockaddr *)&dest_info, sizeof(dest_info));
-    pclose(sock);
+    if (sendto(sock, ip, ntohs(ip->tot_len), 0, (struct sockaddr *)&dest_info, sizeof(dest_info)) == -1)
+    {
+        fprintf(stderr, "sendto() failed with error: %d\n", errno);
+        return -1;
+    }
+    printf("Sent a packet from fake source IP: %s to destination: %s\n", newSourceIP, destinationIP);
+    close(sock);
 }
